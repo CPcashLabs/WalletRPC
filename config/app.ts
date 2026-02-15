d9e999f5c121a083cac9345c8a729bcce9b0261b
lucky <s@merypto.com>
2026-02-16 03:08:56 +0800

    chore(release): bump version to 0.0.2

--- config/app.ts
+++ config/app.ts
@@ -2,4 +2,4 @@
 export const APP_NAME = "Wallet RPC";
 export const SEO_APP_NAME = "CPcash Wallet RPC";
 export const TAGLINE = "High-performance RPC Wallet (Beta)";
-export const APP_VERSION = "0.0.1";
+export const APP_VERSION = "0.0.2";
